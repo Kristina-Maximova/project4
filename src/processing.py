abf8cacca2a1d7ea99ad3335ad73865152ba5585
Kristina Maximova <maximovaki@gmail.com>
2024-12-04 15:28:06 +0300

    add README file

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,9 @@
 from typing import List
+
 from src.widget import get_date
 
 
-def filter_by_state(list_of_dict: List[dict], state="EXECUTED") -> list[dict]:
+def filter_by_state(list_of_dict: List[dict], state: str ="EXECUTED") -> list[dict]:
     """Функция для фильтрации списка словарей по ключу state,
     значение ключа по-умолчанию 'EXECUTED'"""
     new_list_of_dict = []
@@ -13,17 +14,33 @@ def filter_by_state(list_of_dict: List[dict], state="EXECUTED") -> list[dict]:
     return new_list_of_dict
 
 
-def sort_by_date(list_of_dict: List[dict], reverse=True) ->List[dict]:
+def sort_by_date(list_of_dict: List[dict], reverse: bool =True) -> List[dict]:
     """Функция для сортировки списка словарей по дате"""
-    list_of_date = sorted(list_of_dict, key=lambda i: list(map(int, get_date(i["date"]).split('.')))[::-1], reverse=reverse)
+    list_of_date = sorted(
+        list_of_dict, key=lambda x: list(map(int, get_date(x["date"]).split(".")))[::-1], reverse=reverse
+    )
     return list_of_date
 
 
-
 if __name__ == "__main__":
-    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-                          state="CANCELED"))
-    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            state="CANCELED",
+        )
+    )
+    print(
+        sort_by_date(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )

882b005ad204b81325d92cfc23a5b8bf90f476b5
Kristina Maximova <maximovaki@gmail.com>
2024-12-24 22:24:19 +0300

    editing func log in decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,40 +1,52 @@
-from idlelib.iomenu import encoding
-
-from encodings.utf_16 import encode
-from fileinput import filename
 import os
 from functools import wraps
+from time import time
+from datetime import date
 
 
 # import logging
 
-def log(filename: str = ""):
+
+def log(filename: str = "") -> [callable(callable(any))]:
+    """ Декоратор для вывода данных о работе функции."""
+
     def my_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            date_ = date.today()
+            start_time = time()
+            start_info = f"Function {func.__name__} started {date_} at {start_time}"
+
             try:
                 result = func(*args, **kwargs)
                 info = f"{func.__name__} ok"
 
-
             except Exception as exc:
-                info = f"{func.__name__} error: {exc}. Inputs: {args}, {kwargs}"
+                info = f"{func.__name__} error: {exc}. Inputs: {args}, {kwargs}\n"
                 result = None
             # else:
             # info = f"{func.__name__} ok"
             # return result
 
-            if filename:
-                os.makedirs("logs", exist_ok=True)
-                filepath = os.path.join("logs", f"{filename}.txt")
+            finally:
+                end_time = time()
+                running_time = end_time - start_time  # надо  разобраться, как округлять
 
-                with open(filepath, "a", encoding="utf-8") as file:
-                    file.write(f"{info}\n")
-            else:
-                print(info)
+                end_info = f"{func.__name__} running time: {running_time}\n"
 
+                if filename:
+                    os.makedirs("logs", exist_ok=True)
+                    filepath = os.path.join("logs", f"{filename}.txt")
 
+                    with open(filepath, "a", encoding="utf-8") as file:
+                        file.write(start_info)
+                        file.write(f"{info}\n")
+                        file.write(end_info)
 
+                else:
+                    print(start_info)
+                    print(info)
+                    print(end_info)
             return result
 
         return wrapper
@@ -42,10 +54,10 @@ def log(filename: str = ""):
     return my_decorator
 
 
-@log("log")
+@log()
 def division(a, b):
     return a / b
 
 
-x = division(8, 0)
+x = division(8, 4)
 print(x)

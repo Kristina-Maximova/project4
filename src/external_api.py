06836e64b5557ef2d85d082bf9410d96aec50135
Kristina Maximova <maximovaki@gmail.com>
2025-01-01 20:57:57 +0300

    add func in external_api.py and utils.py

---
+++ src/external_api.py
@@ -0,0 +1,45 @@
+import os
+import json
+import requests
+import requests.exceptions
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+url = "https://api.apilayer.com/exchangerates_data/convert"
+
+
+def get_converted_amount(transaction: dict) -> float:
+    """Функция, возвращающая сумму транзакции в рублях."""
+    amount = transaction["operationAmount"]["amount"]  # сумма в формате строки
+    code_of_currency = transaction["operationAmount"]["currency"]["code"]  # код валюты в формате строки
+
+    if code_of_currency != "RUB":
+        try:
+            payload = {
+                "amount": f"{amount}",
+                "from": f"{code_of_currency}",
+                "to": "RUB"
+            }
+            headers = {
+                "apikey": f"{API_KEY}"
+            }
+
+            response = requests.get(url, headers=headers, params=payload)
+
+            status_code = response.status_code
+            if status_code == 200:
+                data = response.json()  # ответ от сервера,
+                return round(data["result"], 2)
+
+        except requests.exceptions.RequestException:
+            print("Ошибка конвертации")
+            return 0
+
+
+if __name__ == "__main__":
+    a = get_converted_amount({'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572',
+                              'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}},
+                              'description': 'Перевод организации', 'from': 'Счет 75106830613657916952',
+                              'to': 'Счет 11776614605963066702'})
+    print(a)
---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import os.path
+import json
+
+
+# Создаем абсолютный путь к файлу
+path_to_file = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
+
+def get_transactions(path: str) -> list[dict]:
+    """Функция для чтения данных о транзакциях из json-файла"""
+    try:
+        with open(path, encoding="utf-8") as transactions_file:
+            try:
+                transactions_data = json.load(transactions_file)
+
+            except json.JSONDecodeError:
+                print("Ошибка декодирования файла")
+                return []
+
+    except FileNotFoundError:
+        print("Файл не найден")
+        return []
+
+    if not transactions_data:
+        return []
+    else:
+        return transactions_data
+
+
+
+
+if __name__ == "__main__":
+
+    transactions_ = get_transactions(path_to_file)
+    print(transactions_)
+

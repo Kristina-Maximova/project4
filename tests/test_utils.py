46f01bc00b00093b19ab482009280de3e4d93650
Kristina Maximova <maximovaki@gmail.com>
2025-01-02 01:19:49 +0300

    additions in tests for utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,21 +1,39 @@
 import json
-from unittest.mock import patch
+from unittest.mock import Mock, patch
 from src.utils import get_transactions
 import os.path
-path_to_file = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
 
+path_to_file = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
 
 
 @patch('json.load')
 def test_get_transactions_with_success(mock_transactions):
-    mock_transactions.return_value = [{"test": "1"} ,{"test": "2"}]
+    """Тест на корректную работу"""
+    mock_transactions.return_value = [{"test": "1"}, {"test": "2"}]
     test_transactions = get_transactions(path_to_file)
-    assert test_transactions == [{"test": "1"} ,{"test": "2"}]
+    assert test_transactions == [{"test": "1"}, {"test": "2"}]
     mock_transactions.assert_called
 
 
-def test_get_transactions_with_invalid_path():
+def test_get_transactions_with_invalid_path(capsys):
+    """Тест при отсутствии файла для чтения или неверном пути """
     test_transactions1 = get_transactions("wrong_path")
+    captured = capsys.readouterr()
     assert test_transactions1 == []
+    assert (captured.out) == "Файл не найден\n"
 
 
+def test_get_transactions_with_invalid_data(capsys):
+    """Тест при неуспешном декодировании файла"""
+    test_transactions1 = get_transactions("test_utils.py")
+    captured = capsys.readouterr()
+    assert test_transactions1 == []
+    assert (captured.out) == "Ошибка декодирования файла\n"
+
+
+def test_get_transactions_with_empty_data():
+    """Тест на работу функции с пустым списком"""
+    mock_data = Mock(return_value=[])
+    json.load = mock_data
+    assert get_transactions(path_to_file) == []
+    mock_data.assert_called()

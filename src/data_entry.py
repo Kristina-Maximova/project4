6570d6bcee268c626975249e38c1573e653aaeda
Kristina Maximova <maximovaki@gmail.com>
2025-01-13 22:38:05 +0300

    add one more func in data_entry.py

--- src/data_entry.py
+++ src/data_entry.py
@@ -1,12 +1,11 @@
 import csv
 import os
 
-from mypy.util import DecodeError
-
-from tests.conftest import transactions
+import pandas as pd
 
 path_to_current_file = os.path.dirname(os.path.abspath(__file__))
 path_to_csv_file = os.path.join(path_to_current_file, "..", "data", "transactions.csv")
+path_to_excel_file = os.path.join(path_to_current_file, "..", "data", "transactions_excel.xlsx")
 
 
 def get_trensactions_from_csv_file(path_to_file: str) -> list | list[dict]:
@@ -20,11 +19,26 @@ def get_trensactions_from_csv_file(path_to_file: str) -> list | list[dict]:
                     return []
         except Exception as e:
             print(f"Ошибка при чтении csv-файла: {e}")
+            return []
     except FileNotFoundError:
         print("csv-файл не найден ")
         return []
 
 
+def get_transactions_from_exel_file(path_to_file: str) -> list[dict] | list:
+    try:
+        excel_data = pd.read_excel(path_to_file)
+        if not excel_data.empty:
+            return list(excel_data.to_dict(orient="records"))
+        else:
+            return []
+    except Exception as e:
+        print(f"Ошибка при чтении ecxell-файла: {e}")
+        return []
+
+
 if __name__ == "__main__":
-    transactions_1 = get_trensactions_from_csv_file(path_to_csv_file)
-    print(transactions_1[-5:])
+    # transactions_1 = get_trensactions_from_csv_file(path_to_csv_file)
+    # print(transactions_1[-5:])
+    transactions_2 = get_transactions_from_exel_file(path_to_excel_file)
+    print(transactions_2[-5:])

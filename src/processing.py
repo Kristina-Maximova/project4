64eef365955a361200780c299264ac41620187d1
Kristina Maximova <maximovaki@gmail.com>
2025-01-15 20:48:42 +0300

    add sort_by_description func

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,10 @@
+import re
+
 from src.widget import get_date
+from tests.conftest import transactions
 
 
-def filter_by_state(list_of_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(list_of_dict: list[dict], state: str = "EXECUTED") -> list[dict] | list:
     """Функция для фильтрации списка словарей по ключу state,
     значение ключа по-умолчанию 'EXECUTED'"""
     new_list_of_dict = []
@@ -12,7 +15,7 @@ def filter_by_state(list_of_dict: list[dict], state: str = "EXECUTED") -> list[d
     return new_list_of_dict
 
 
-def sort_by_date(list_of_dict: list[dict], reverse: bool = True) -> list[dict]:
+def sort_by_date(list_of_dict: list[dict], reverse: bool = True) -> list[dict] | list:
     """Сортирует список словарей по дате"""
     if not list_of_dict:
         return []
@@ -26,25 +29,68 @@ def sort_by_date(list_of_dict: list[dict], reverse: bool = True) -> list[dict]:
         return list_of_date
 
 
-# if __name__ == "__main__":
-#     print(
-#         filter_by_state(
-#             [
-#                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-#             ],
-#             state="CANCELED",
-#         )
-#     )
-#     print(
-#         sort_by_date(
-#             [
-#                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-#             ]
-#         )
-#     )
+def sort_by_description(list_of_dict: list[dict], keyword: str) -> list | list[dict]:
+    if not list_of_dict:
+        return []
+    else:
+        # filtered_data = []
+        # for transaction in list_of_dict:
+        #     if re.search(keyword, transaction["description"], flags=re.IGNORECASE):
+        #         filtered_data.append(transaction)
+        try:
+            return  [transaction for transaction in list_of_dict  if re.search(keyword, transaction["description"], flags=re.IGNORECASE)]
+        except Exception as e:
+            print(f"Ошибка при фильтрации по ключевому слову в описании: {e}")
+            return []
+
+if __name__ == "__main__":
+    keyword = input("Введите слово для фильтрации: \n").lower()
+    data =  [
+            {
+                "id": 939719570,
+                "state": "EXECUTED",
+                "date": "2018-06-30T02:08:58.425572",
+                "operationAmount": {
+                    "amount": "9824.07",
+                    "currency": {
+                        "name": "USD",
+                        "code": "USD"
+                    }
+                },
+                "description": "Перевод организации",
+                "from": "Счет 75106830613657916952",
+                "to": "Счет 11776614605963066702"
+            },
+            {
+                "id": 142264268,
+                "state": "EXECUTED",
+                "date": "2019-04-04T23:20:05.206878",
+                "operationAmount": {
+                    "amount": "79114.93",
+                    "currency": {
+                        "name": "USD",
+                        "code": "USD"
+                    }
+                },
+                "description": "Перевод со счета на счет",
+                "from": "Счет 19708645243227258542",
+                "to": "Счет 75651667383060284188"
+            },
+            {
+                "id": 873106923,
+                "state": "EXECUTED",
+                "date": "2019-03-23T01:09:46.296404",
+                "operationAmount": {
+                    "amount": "43318.34",
+                    "currency": {
+                        "name": "руб.",
+                        "code": "RUB"
+                    }
+                },
+                "description": "Перевод со счета на счет",
+                "from": "Счет 44812258784861134719",
+                "to": "Счет 74489636417521191160"
+            }]
+
+    filtered_data = sort_by_description(data, keyword)
+    print(filtered_data)
